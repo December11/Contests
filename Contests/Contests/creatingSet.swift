78e35730e95c79e84cc1f41b4c701b1761058daa
Alla Shkolnik <5106275+December11@users.noreply.github.com>
2022-11-26 18:16:18 +0200

    counting sort

---
+++ Contests/Contests/creatingSet.swift
@@ -0,0 +1,65 @@
+//
+//  creatingSet.swift
+//  Contests
+//
+//  Created by 👩🏻‍🎨 📱 december11 on 26.11.2022.
+//
+
+import Foundation
+/*
+struct MySet {
+    
+    private var elements = [[Int]]()
+    private let setSize = 10
+    
+    init(elements : [[Int]] = [[]]) {
+        if elements == [[]] {
+            for _ in 0...self.setSize {
+                self.elements.append([Int]())
+            }
+        } else {
+            self.elements = elements
+        }
+    }
+    
+    mutating func add(_ element: Int) {
+        guard !find(element) else { return }
+        elements[element % setSize].append(element)
+    }
+    
+    mutating func delete(_ element: Int) -> Bool {
+        guard find(element) else { return false }
+        for i in 0 ..< elements[element % setSize].count {
+            if elements[element % setSize][i] == element,
+               let last = elements[element % setSize].last {
+                elements[element % setSize][i] = last
+            }
+        }
+        elements[element % setSize].removeLast()
+        
+        return true
+    }
+    
+    func find(_ element: Int) -> Bool {
+        elements[element % setSize].contains(element)
+    }
+}
+
+var set1 = MySet()
+set1.add(12)
+set1.add(23)
+set1.add(22)
+set1.add(25)
+set1.add(2)
+set1.add(17)
+
+print(set1)
+print()
+print(set1.find(12))
+print(set1.find(24))
+print()
+print(set1.delete(12))
+print(set1.delete(12))
+print(set1)
+
+*/
--- Contests/Contests/main.swift
+++ Contests/Contests/main.swift
@@ -7,62 +7,31 @@
 
 import Foundation
 
-////// Задача
-///Реализовать работу множества
-///хеш-функция X % 10
+////// Задача 3
+/// Реализовать сортировку подсчетом
+///
 
-struct MySet {
+func countingSorted(_ array: [Int]) -> [Int] {
+    guard
+        let minValue = array.min(),
+        let maxValue = array.max()
+    else { return [] } // зачем тут мин и макс?
+    var results = array
     
-    private var elements = [[Int]]()
-    private let setSize = 10
+    let possibleElementsCount = maxValue - minValue + 1 // why +1? зачем вычислять разницу между мин и макс?
+                                    // вычисляем тут количество возможных элементов.
+    var countValues = Array(repeating: 0, count: possibleElementsCount)
     
-    init(elements : [[Int]] = [[]]) {
-        if elements == [[]] {
-            for _ in 0...self.setSize {
-                self.elements.append([Int]())
-            }
-        } else {
-            self.elements = elements
+    for element in array {
+        let index = element - minValue
+        countValues[index] = countValues[index] + 1
+    }
+    var nowPosition = 0
+    for element in 0 ..< possibleElementsCount {
+        for _ in 0..<countValues[element] {
+            results[nowPosition] = element + minValue
+            nowPosition += 1
         }
     }
-    
-    mutating func add(_ element: Int) {
-        guard !find(element) else { return }
-        elements[element % setSize].append(element)
-    }
-    
-    mutating func delete(_ element: Int) -> Bool {
-        guard find(element) else { return false }
-        for i in 0 ..< elements[element % setSize].count {
-            if elements[element % setSize][i] == element,
-               let last = elements[element % setSize].last {
-                elements[element % setSize][i] = last
-            }
-        }
-        elements[element % setSize].removeLast()
-        
-        return true
-    }
-    
-    func find(_ element: Int) -> Bool {
-        elements[element % setSize].contains(element)
-    }
+    return results 
 }
-
-var set1 = MySet()
-set1.add(12)
-set1.add(23)
-set1.add(22)
-set1.add(25)
-set1.add(2)
-set1.add(17)
-
-print(set1)
-print()
-print(set1.find(12))
-print(set1.find(24))
-print()
-print(set1.delete(12))
-print(set1.delete(12))
-print(set1)
-
