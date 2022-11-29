114fcc0311b489376089957557d600a487a9d7d9
december11 <5106275+December11@users.noreply.github.com>
2022-11-29 16:40:28 +0200

    202. Happy Number

--- Contests/Contests/main.swift
+++ Contests/Contests/main.swift
@@ -7,29 +7,36 @@
 
 import Foundation
 
-////// 88. Merge Sorted Array
-/// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
-/// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
-/// The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+////// 202. Happy Number
+/// Write an algorithm to determine if a number n is happy.
+/// A happy number is a number defined by the following process:
+
+/// Starting with any positive integer, replace the number by the sum of the squares of its digits.
+/// Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+/// Those numbers for which this process ends in 1 are happy.
+/// Return true if n is a happy number, and false if not.
 
 class Solution {
-    func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
-        var first = 0
-        var last = 0
-        var minValue = 0
-        var array = nums1
-        for i in 0..<(n+m) {
-            while first < nums1.count && last < nums2.count {
-                if nums2[last] >= nums1[first] {
-                    nums1.insert(nums2[last], at: i)
-                    last += 1
-                } else {
-                    first += 1
-                }
-            }
-            if m > n {
-                array[i] = nums2[i]
-            }
+    func isHappy(_ n: Int) -> Bool {
+        var set = Set<Int>()
+        var number = n
+        while number != 1 && !set.contains(number) {
+            set.insert(number)
+            number = squareSum(number)
         }
+        
+        return number == 1
+    }
+    
+    func squareSum(_ n: Int) -> Int {
+        var num = n
+        var sum = 0
+        while num > 0 {
+            let lastDigit = num % 10
+            sum += (lastDigit * lastDigit)
+            num /= 10
+        }
+        
+        return sum
     }
 }
