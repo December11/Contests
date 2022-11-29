136474d27faee38cc1d58be8bdbaf7991591582f
december11 <5106275+December11@users.noreply.github.com>
2022-11-29 16:39:13 +0200

    88. Merge Sorted Array

--- Contests/Contests/main.swift
+++ Contests/Contests/main.swift
@@ -7,36 +7,29 @@
 
 import Foundation
 
-////// 242. Valid Anagram
-/// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
-/// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
-///
+////// 88. Merge Sorted Array
+/// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+/// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+/// The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 
 class Solution {
-    func isAnagram(_ s: String, _ t: String) -> Bool {
-        // 1. создать два словаря
-        // 2. добавить все символы первого слова в первый словарь
-        // 3. добавить все символы второго слова во второй словарь
-        // 4. проверить совпадает ли массив ключей.
-        
-        func makeDictionary(_ s: String) -> [Character: Int] {
-            var results = [Character: Int]()
-            for char in s {
-                if results.keys.contains(char) {
-                    if let oldValue = results[char] {
-                        results[char] = oldValue + 1
-                    }
+    func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+        var first = 0
+        var last = 0
+        var minValue = 0
+        var array = nums1
+        for i in 0..<(n+m) {
+            while first < nums1.count && last < nums2.count {
+                if nums2[last] >= nums1[first] {
+                    nums1.insert(nums2[last], at: i)
+                    last += 1
                 } else {
-                    results[char] = 1
+                    first += 1
                 }
             }
-            
-            return results
+            if m > n {
+                array[i] = nums2[i]
+            }
         }
-        
-        let sDict = makeDictionary(s)
-        let tDict = makeDictionary(t)
-        
-        return sDict == tDict
     }
 }
