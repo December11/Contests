0af1d973a6635e0d16c93b17f928a879057b066e
december11 <5106275+December11@users.noreply.github.com>
2022-11-29 16:35:45 +0200

    374. Guess Number Higher or Lower

--- Contests/Contests/main.swift
+++ Contests/Contests/main.swift
@@ -7,29 +7,44 @@
 
 import Foundation
 
-////// Задача
-/// Игрок в футбол обладает одной числовой характеристикой - профессионализм.
-/// команда называется сплоченной, если профессионализм одного игрока не превосходмт суммарный проф. двух любых других игроков команды.
-/// Команда состоит из любого числа игроков.
-/// Дана отсортированная последовательность чисел длинной N - професионализм игроков.
+////// 374. Guess Number Higher or Lower
+/// We are playing the Guess Game. The game is as follows:
+/// I pick a number from 1 to n. You have to guess which number I picked.
+/// Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
 ///
-/// необходимо найти макс. суммарный професионализм сплоченной команды.
+/// You call a pre-defined API int guess(int num), which returns three possible results:
 ///
+/// -1: Your guess is higher than the number I picked (i.e. num > pick).
+/// 1: Your guess is lower than the number I picked (i.e. num < pick).
+/// 0: your guess is equal to the number I picked (i.e. num == pick).
+/// Return the number that I picked.
 
-func bestTeamSum(players: [Int]) -> Int {
-    var bestSum = 0
-    var nowSum = 0
-    var last = 0
-    for first in 0..<players.count {
-        while last < players.count &&
-                (last == first || players[first] + players[first+1] >= players[last]) {
-            nowSum += players[last]
-            last += 1
+
+/**
+ * Forward declaration of guess API.
+ * @param  num -> your guess number
+ * @return          -1 if num is higher than the picked number
+ *                  1 if num is lower than the picked number
+ *               otherwise return 0
+ * func guess(_ num: Int) -> Int
+ */
+
+class Solution : GuessGame {
+    func guessNumber(_ n: Int) -> Int {
+        var left = 1
+        var right = n
+        var mid = 0
+        while left <= right {
+            mid = (left + right) / 2
+            let answer = guess(mid)
+            if answer == 0 {
+                return mid
+            } else if answer == 1 {
+                left = mid + 1
+            } else {
+                right = mid
+            }
         }
-        
-        bestSum = max(bestSum, nowSum)
-        nowSum -= players[first]
+        return mid
     }
-    
-    return bestSum
 }
